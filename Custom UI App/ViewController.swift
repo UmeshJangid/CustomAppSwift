faf3482e362061156067ffb5031f25abcce9eab4
UmeshJangid <er.umeshjangid@gmail.com>
2022-01-10 11:31:50 +0530

    Inital Commit with Swift Learning.

---
+++ Custom UI App/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Custom UI App
+//
+//  Created by Macbook Air  on 26/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var leftDicePostion  = 1
+    var rightDicePostion  = 5
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray =  [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+    }
+    
+}
+
